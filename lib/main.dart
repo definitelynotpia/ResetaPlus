b47004f77f0bcc8e84ffe3fe22742baffd5be2e3
Frances Pia Alexy Rey <frances.rey@ciit.edu.ph>
2024-09-18 20:32:58 +0800

    Initial login page with email and password fields, remember-me checkbox (to be customized)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gradient_borders/gradient_borders.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,216 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: "Reseta+",
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
+      home: const LandingPage(title: "Home"),
     );
+  }
+}
+
+class LandingPage extends StatefulWidget {
+  const LandingPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<StatefulWidget> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  // generate global key, uniquely identify Form widget and allow form validation
+  final _formKey = GlobalKey<FormState>();
+
+  // test login credentials
+  // final String _testEmail = "admin@google.com";
+  final String _testPassword = "qwerty";
+
+  // store input field values
+  // ignore: unused_field
+  String? _email;
+  // ignore: unused_field
+  String? _password;
+
+  // Initially password is obscure
+  bool _obscureText = true;
+  // Remember user
+  bool _rememberUser = false;
+
+  // Toggles the password show status
+  void _toggleObscuredText() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
+
+  // ui here
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: const Color(0xffF8F6F5),
+        body: Form(
+          key: _formKey,
+          child: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                      child: Container(
+                    constraints: BoxConstraints(
+                      minWidth: MediaQuery.sizeOf(context).width,
+                      maxWidth: MediaQuery.sizeOf(context).width,
+                    ),
+                    child: Padding(
+                        padding: EdgeInsets.only(
+                            left: MediaQuery.sizeOf(context).width / 10,
+                            right: MediaQuery.sizeOf(context).width / 10),
+                        child: Column(
+                          children: [
+                            Padding(
+                                padding:
+                                    const EdgeInsets.only(top: 50, bottom: 50),
+                                child:
+                                    Image.asset('assets/logo_ResetaPlus.png')),
+                            // Email input field
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 10),
+                              child: TextFormField(
+                                cursorColor:
+                                    Theme.of(context).colorScheme.primary,
+                                decoration: const InputDecoration(
+                                    border: GradientOutlineInputBorder(
+                                        gradient: LinearGradient(colors: [
+                                          Color(0xffa16ae8),
+                                          Color(0xff94b9ff)
+                                        ]),
+                                        width: 2.0),
+                                    prefixIcon: Icon(
+                                      Icons.person,
+                                      color: Color(0xFFa16ae8),
+                                    ),
+                                    label: Text("Email")),
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return "Email cannot be empty.";
+                                  }
+                                  return null;
+                                },
+                                onSaved: (value) => _email = value,
+                              ),
+                            ),
+                            // Password input field
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 10, bottom: 10),
+                              child: TextFormField(
+                                cursorColor:
+                                    Theme.of(context).colorScheme.primary,
+                                decoration: InputDecoration(
+                                  border: const GradientOutlineInputBorder(
+                                      gradient: LinearGradient(colors: [
+                                        Color(0xffa16ae8),
+                                        Color(0xff94b9ff)
+                                      ]),
+                                      width: 2.0),
+                                  label: const Text("Password"),
+                                  prefixIcon: const Icon(
+                                    Icons.lock,
+                                    color: Color(0xFFa16ae8),
+                                  ),
+                                  suffixIcon: IconButton(
+                                      icon: Icon(
+                                        _obscureText
+                                            ? Icons.visibility
+                                            : Icons.visibility_off,
+                                        color: const Color(0xFFa16ae8),
+                                      ),
+                                      onPressed: _toggleObscuredText),
+                                ),
+                                obscureText: _obscureText,
+                                enableSuggestions: false,
+                                autocorrect: false,
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return "Password cannot be empty.";
+                                  } else if (value == _testPassword) {
+                                    return null;
+                                  }
+                                  return "Password is incorrect.";
+                                },
+                                onSaved: (value) => _password = value,
+                              ),
+                            ),
+                            Padding(
+                                padding:
+                                    const EdgeInsets.only(top: 10, bottom: 20),
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Expanded(
+                                      child: TextFormField(
+                                        style: TextStyle(
+                                            decoration: _rememberUser
+                                                ? TextDecoration.lineThrough
+                                                : TextDecoration.none),
+                                        decoration: InputDecoration(
+                                            border: InputBorder.none,
+                                            contentPadding: const EdgeInsets.only(top: 13),
+                                            prefixIcon: Checkbox(
+                                                value: _rememberUser,
+                                                onChanged: (value) {
+                                                  setState(() {
+                                                    _rememberUser = value!;
+                                                  });
+                                                }),
+                                            hintText: 'Remember me'),
+                                      ),
+                                    ),
+                                    const Flexible(
+                                      fit: FlexFit.tight,
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.stretch,
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Text(
+                                            '+100',
+                                            maxLines: 1,
+                                            softWrap: false,
+                                            overflow: TextOverflow.fade,
+                                          ),
+                                          Text(
+                                            '18 Sept 2021',
+                                            maxLines: 1,
+                                            softWrap: false,
+                                            overflow: TextOverflow.fade,
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                )),
+                            ElevatedButton(
+                              onPressed: () {
+                                if (_formKey.currentState!.validate()) {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(
+                                          content: Text("Processing Data")));
+                                }
+                              },
+                              child: const Text("Login"),
+                            ),
+                          ],
+                        )),
+                  ))
+                ]),
+          ),
+        ));
   }
 }
