7a5b547d880182740b1a861ff7e49e7de313fd51
VonRoderno <von.roderno@ciit.edu.ph>
2024-10-28 20:27:20 +0800

    Added doctor_account_keys, doctor_account and verified_license tables

--- database/reseta_plus.sql
+++ database/reseta_plus.sql
@@ -108,10 +108,59 @@ CREATE TABLE `patient_prescriptions` (
   `refills` varchar(255) NOT NULL,
   `status` varchar(255) NOT NULL,
   `intake_instructions` varchar(255) NOT NULL,
+  `doctor_id` int NOT NULL,
   PRIMARY KEY (`prescription_id`),
   UNIQUE KEY `prescription_id_UNIQUE` (`prescription_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `doctor_account_keys`
+--
+
+DROP TABLE IF EXISTS `doctor_account_keys`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `doctor_account_keys` (
+  `doctor_key_id` int NOT NULL AUTO_INCREMENT,
+  `encryption_key` varchar(255) DEFAULT NULL,
+  `initialization_vector` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`doctor_key_id`),
+  UNIQUE KEY `doctor_key_id_UNIQUE` (`doctor_key_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `doctor_accounts`
+--
+
+DROP TABLE IF EXISTS `doctor_accounts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `doctor_accounts` (
+  `doctor_id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `license_number` varchar(255) DEFAULT NULL,
+  `salt` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`doctor_id`),
+  UNIQUE KEY `user_id_UNIQUE` (`doctor_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `verified_license`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `verified_license` (
+  `license_id` int NOT NULL AUTO_INCREMENT,
+  `license_number` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`license_id`),
+  UNIQUE KEY `user_id_UNIQUE` (`license_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
